2c38b3b2d5914033b65ebbcee3b6858c656a63c0
juana Meza <juana.meza@usach.cl>
2023-06-09 01:11:13 -0400

    agregandorutas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+  get "/home", to: 'news#index'
+  get "/projects", to: 'news#index'
+  get "/contact", to: 'news#index'
